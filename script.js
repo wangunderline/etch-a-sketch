7bd5072f3755fc03ffa9048c40480865dfcc525b
wangunderline <wangwangrian@gmail.com>
2024-03-14 19:52:58 -0300

    Resize grid with function parameters
    Grid logic was replaced into createGrid, making possible to manipulate the grid size with the squareNum parameter, which is set in the addEventListener.
    
    The addEventListener below createGrid remove the previous grid with a while loop to create another one based in the value received in the prompt. Then the createGrid function is called again with the prompt value as an argument.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const container = document.querySelector('#container')
+const container = document.querySelector("#main-container");
+const gridContainer = document.querySelector("#grid-container");
+const button = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement('div')
-  grid.classList.add('grid')
-  container.appendChild(grid)
+let userPrompt = 256;
+
+function createGrid (squareNum) {
+  for (let i = 0; i < squareNum; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add('grid')
+    gridContainer.appendChild(grid);
+
+    for (let i = 0; i < squareNum; i++) {
+      const square = document.createElement('div')
+      square.classList.add('square')
+      grid.appendChild(square)
+    }
+  }
 }
+
+createGrid(16)
+
+button.addEventListener("click", () => {
+  while (gridContainer.hasChildNodes()) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  const userPrompt = prompt("The fuck?");
+  createGrid(userPrompt);
+});
