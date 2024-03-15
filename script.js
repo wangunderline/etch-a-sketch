78ad378377e159af05f04150e4716167eb956957
wangunderline <wangwangrian@gmail.com>
2024-03-14 21:53:42 -0300

    Randomize background color when hovering squares
    Three Math.randoms were needed to achieve three different values for the rgb, as they would be the same if only one were used.

--- script.js
+++ script.js
@@ -2,28 +2,36 @@ const container = document.querySelector("#main-container");
 const gridContainer = document.querySelector("#grid-container");
 const button = document.querySelector("button");
 
-function createGrid (gridSize) {
+function hoverOnGrid() {
+  let allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      let rgb1 = Math.floor(Math.random() * 255) + 0;
+      let rgb2 = Math.floor(Math.random() * 255) + 0;
+      let rgb3 = Math.floor(Math.random() * 255) + 0;
+      let rgbValue = `${rgb1}, ${rgb2}, ${rgb3}`;
+      square.style.backgroundColor = `rgb(${rgbValue})`;
+    });
+  });
+}
+
+function createGrid(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     const grid = document.createElement("div");
-    grid.classList.add('grid')
+    grid.classList.add("grid");
     gridContainer.appendChild(grid);
 
     for (let j = 0; j < gridSize; j++) {
-      const square = document.createElement('div')
-      square.classList.add('square')
-      grid.appendChild(square)
+      const square = document.createElement("div");
+      square.classList.add("square");
+      grid.appendChild(square);
     }
   }
-  let allSquares = document.querySelectorAll('.square')
-  allSquares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-      square.style.filter = 'drop-shadow(5px 5px 15px red)'
-      square.style.backgroundColor = 'red'
-    })
-  })
+  hoverOnGrid();
 }
 
-createGrid(16)
+createGrid(16);
+
 
 button.addEventListener("click", () => {
   while (gridContainer.hasChildNodes()) {
@@ -32,4 +40,3 @@ button.addEventListener("click", () => {
   const userPrompt = prompt("Grid size");
   createGrid(userPrompt);
 });
-
